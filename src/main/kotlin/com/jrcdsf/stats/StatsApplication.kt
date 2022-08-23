9b2ca023ab7d7f3c8d5938d4bde4a29746794990
jrcdsf <jrcdsf@gmail.com>
2022-08-23 10:15:46 -0300

    initial version

---
+++ src/main/kotlin/com/jrcdsf/stats/StatsApplication.kt
@@ -0,0 +1,11 @@
+package com.jrcdsf.stats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatsApplication>(*args)
+}
---
+++ src/test/kotlin/com/jrcdsf/stats/StatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jrcdsf.stats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
