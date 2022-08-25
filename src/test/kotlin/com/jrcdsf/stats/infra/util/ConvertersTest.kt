369c108bedd6421cd230a45dd9306f45a2f4dd02
jrcdsf <jrcdsf@gmail.com>
2022-08-25 16:17:35 -0300

    add missing unit test

---
+++ src/test/kotlin/com/jrcdsf/stats/infra/util/ConvertersTest.kt
@@ -0,0 +1,28 @@
+package com.jrcdsf.stats.infra.util
+
+import com.jrcdsf.stats.domain.entities.Event
+import com.jrcdsf.stats.util.Helper
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.time.Instant
+
+internal class ConvertersTest {
+
+    @Test
+    fun `convertBodyToEvents should convert a string body into a list of events`() {
+
+        val body = Helper.generateEventsBody(3, 1)
+        val lines = body.split("\r?\n|\r".toRegex()).toTypedArray()
+        val eventList = lines.flatMap {
+            try {
+                val (timestamp, x, y) = it.split(",")
+                listOf(Event(timestamp = Instant.ofEpochMilli(timestamp.toLong()), x = x.toDouble(), y = y.toInt()))
+            } catch (e: Exception) {
+                emptyList()
+            }
+        }
+
+        assertIterableEquals(eventList, Converters.convertBodyToEvents(body))
+    }
+}
